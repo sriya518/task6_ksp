3c74596638d884d8b90c711f996b9137728ff029
sriya518 <114557105+sriya518@users.noreply.github.com>
2023-10-15 16:27:31 +0530

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,43 @@
+function reading(){
+    console.log("sriya is reading");
+}
+
+function writing(){
+    console.log("sriya is writing");
+}
+
+function listening(){
+    console.log("sriya is listening");
+}
+
+
+reading();
+
+
+function playing(){
+    console.log("children are playing");
+}
+
+playing();
+
+let digit1 = 1
+let digit2 = 4
+let digit3 = 3
+
+function addition(){
+    console.log(digit1+digit2);
+}
+
+addition();
+
+function subtraction(){
+    console.log(digit1-digit2);
+}
+
+subtraction();
+
+console.log("hey guys")
+
+for(let i=0; i<10; i++){
+    console.log("hey  guys")
+}
